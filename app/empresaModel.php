4b9995b1405841136123622104be0d2f908c7995
Claudio-Salvador <claudiosalvador1997@gmail.com>
2021-04-27 00:44:23 +0100

    modelo empresa

--- app/empresaModel.php
+++ app/empresaModel.php
@@ -3,8 +3,20 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class empresaModel extends Model
 {
-    //
+    protected $table='empresas';
+
+    protected $fillable = [
+        'empresa', 'descricao', 'email', 'localidade',
+    ];
+
+
+    public function setEmpresaAttribute($value){
+
+            $this->attributes['empresa']=$value;
+            $this->attributes['slug']=Str::slug($value);
+    }
 }
