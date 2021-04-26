66ae5d9af4999481e41dadddcad0b12cf3cb67db
Claudio-Salvador <claudiosalvador1997@gmail.com>
2021-04-26 02:06:41 +0100

    Migrações
    Criando as migrações para o projecto
    #tabela funcionarios
    #tabela Empresas

---
+++ database/migrations/2021_04_26_002320_create_empresas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmpresasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('empresas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('empresa');
+            $table->string('descricao')->nullable();
+            $table->text('slug');
+            $table->string('email')->unique();
+            $table->string('localidade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('empresas');
+    }
+}
